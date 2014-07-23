15bcfa24325e54bf2c101b672892f4acab151fe8
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-23 17:52:33 -0400

    modularize inquiry.js

--- inquiry.js
+++ inquiry.js
@@ -1,25 +1,27 @@
-var app = require('./app');
-var inquirer = require("inquirer");
+module.exports = function () {
+	var app = require('./app');
+	var inquirer = require("inquirer");
 
-var renderables = ['register'];
+	var renderables = ['register'];
 
-inquirer.prompt([
-	{
-		choices: 
-			['service',
-			'directive', 
-			'register', 
-			'routes',
-			'view',
-			'controller'],
-		message: "Which resources would you like?",
-		name: 'choices',
-		type: 'checkbox'
-	},
-	{
-		message: "What is the name of your module?",
-		name: 'mod'
-	}], function( answers ) {
-		renderables += answers.choices;
-		app(answers.mod, renderables)
-	});
+	inquirer.prompt([
+		{
+			choices: 
+				['service',
+				'directive', 
+				'register', 
+				'routes',
+				'view',
+				'controller'],
+			message: "Which resources would you like?",
+			name: 'choices',
+			type: 'checkbox'
+		},
+		{
+			message: "What is the name of your module?",
+			name: 'mod'
+		}], function( answers ) {
+			renderables += answers.choices;
+			app(answers.mod, renderables)
+		});
+}
