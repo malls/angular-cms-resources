dec5cab462300aab9768976f7776c97e2c2cc97c
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-23 13:16:48 -0400

    scaffolding done, templating to do

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-var service = './boilerplates/service.template';
-var serviceTest = './boilerplates/servicetest.template';
-var directive = './boilerplates/directive.template';
-var directiveTest = './boilerplates/directiveTest.template';
-var register = './boilerplates/register.template';
-var routes = './boilerplates/routes.template';
-
-
 var fs = require('fs-extended');
 var express = require('express');
 var app = express();
 
+var renderables = ['service', 'directive', 'register', 'routes', 'view', 'controller'];
 
 function capitalize(str) {
 	return str[0].toUpperCase() + str.slice(1);
@@ -19,25 +12,40 @@ function pluralize(str) {
 	return str + "s";
 }
 
-function render(template, resource, type) {
+function render(type, resource) {
     var resources = pluralize(resource);
     var Resource = capitalize(resource);
-	var path = "./modules/" + resources;
+    var path = "./boilerplates/" + type + ".template";
+	var destination = "./modules/" + resources;
     
-	var tpl = fs.readFileSync(template);
-	
+	var tpl = fs.readFileSync(path);
+
     tpl = tpl + "";
     tpl = tpl.replace(/{{resources}}/g, resources);
     tpl = tpl.replace(/{{Resource}}/g, Resource);
     tpl = tpl.replace(/{{resource}}/g, resource);
-    if (template === register) {
-    	fs.createFileSync(path + "/" + resource + ".js", tpl);
-    }else if (template === routes) {
-    	fs.createFileSync(path + "/routes.js", tpl);
-    } else {
-    	fs.createFileSync(path + "/" + type + "/" + resource + ".js", tpl);
+
+    if (type !== 'view'){
+    	var testPath = "./boilerplates/" + type + "test.template";
+		var testTpl = fs.readFileSync(testPath)
+	    testTpl = testTpl + "";
+	    testTpl = testTpl.replace(/{{resources}}/g, resources);
+	    testTpl = testTpl.replace(/{{Resource}}/g, Resource);
+	    testTpl = testTpl.replace(/{{resource}}/g, resource);
     }
 
+    if (type === 'register') {
+    	fs.createFileSync(destination + "/" + resource + ".js", tpl);
+    	fs.createFileSync(destination + "/tests/" + resource + ".js", testTpl);
+    } else if (type === 'routes') {
+    	fs.createFileSync(destination + "/routes.js", tpl);
+    	fs.createFileSync(destination + "/tests/routes.js", testTpl);    
+    } else if (type === 'view') {
+    	fs.createFileSync(destination + "/views/" + resource + ".html", tpl);
+    } else {
+    	fs.createFileSync(destination + "/" + pluralize(type) + "/" + resource + ".js", tpl);
+    	fs.createFileSync(destination + "/tests/" + pluralize(type) + "/" + resource + ".js", testTpl);
+    }
 
 }
 
@@ -49,10 +57,9 @@ app.post('/delete', function(req, res){
 app.post('/:resource', function(req, res){
     var resource = req.params.resource;
 
-    render(service, resource, 'services');
-    render(directive, resource, 'directives');
-    render(register, resource);
-    render(routes, resource);
+    for (var i = 0; i < renderables.length; i++) {
+    	render(renderables[i], resource);
+    }
 
     res.send(resource);
 });
