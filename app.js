a5d675fc3593a395c633080266604dfd64ee01cf
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-23 12:52:26 -0400

    render abstracted

--- app.js
+++ app.js
@@ -19,23 +19,26 @@ function pluralize(str) {
 	return str + "s";
 }
 
-function render(template, resource) {
+function render(template, resource, type) {
     var resources = pluralize(resource);
     var Resource = capitalize(resource);
 	var path = "./modules/" + resources;
     
 	var tpl = fs.readFileSync(template);
+	
     tpl = tpl + "";
     tpl = tpl.replace(/{{resources}}/g, resources);
     tpl = tpl.replace(/{{Resource}}/g, Resource);
     tpl = tpl.replace(/{{resource}}/g, resource);
-    if (template === routes || template === register) {
-    	fs.createFileSync(path + ".js", tpl);
+    if (template === register) {
+    	fs.createFileSync(path + "/" + resource + ".js", tpl);
     }else if (template === routes) {
-    	fs.createFileSync("routes.js", tpl);
+    	fs.createFileSync(path + "/routes.js", tpl);
     } else {
-    	fs.createFileSync(path + "/" + resources + "/" + resource + ".js", tpl);
+    	fs.createFileSync(path + "/" + type + "/" + resource + ".js", tpl);
     }
+
+
 }
 
 app.post('/delete', function(req, res){
@@ -45,41 +48,12 @@ app.post('/delete', function(req, res){
 
 app.post('/:resource', function(req, res){
     var resource = req.params.resource;
-    var resources = pluralize(resource);
-    var Resource = capitalize(resource);
 
-    var Resource = capitalize(resource);
-
-    var path = "./modules/" + resources;
-
-
-    render(service, resource);
-    render(directive, resource);
+    render(service, resource, 'services');
+    render(directive, resource, 'directives');
     render(register, resource);
     render(routes, resource);
 
-
-    // var regTpl = fs.readFileSync(register);
-    // regTpl = regTpl + "";
-    // regTpl = regTpl.replace(/{{resources}}/g, resources);
-    // regTpl = regTpl.replace(/{{Resource}}/g, Resource);
-    // regTpl = regTpl.replace(/{{resource}}/g, resource);
-    // fs.createFileSync(path + ".js", regTpl);
-
-    // var dirTpl = fs.readFileSync(directive);
-    // dirTpl = dirTpl + "";
-    // dirTpl = dirTpl.replace(/{{resources}}/g, resources);
-    // dirTpl = dirTpl.replace(/{{Resource}}/g, Resource);
-    // dirTpl = dirTpl.replace(/{{resource}}/g, resource);
-    // fs.createFileSync(path + "/directives/" + resource + ".js", dirTpl);
-    
-    // var svcTpl = fs.readFileSync(service);
-    // svcTpl = svcTpl + "";
-    // svcTpl = svcTpl.replace(/{{resources}}/g, resources);
-    // svcTpl = svcTpl.replace(/{{Resource}}/g, Resource);
-    // svcTpl = svcTpl.replace(/{{resource}}/g, resource);
-    // fs.createFileSync(path + "/services/" + resource + ".js", svcTpl);
-    
     res.send(resource);
 });
 
