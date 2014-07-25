bee96111bf059d9ca132f8ffd8aa9bd8810c860c
Forrest Almasi <forrest.almasi@gmail.com>
2014-07-25 13:36:40 -0400

    string validation

--- inquiry.js
+++ inquiry.js
@@ -5,26 +5,29 @@ module.exports = function (callback) {
 	inquirer.prompt([
 		{
 			message: 'What is the name of your module?',
+			name: 'mod',
 			validate: function(input) {
 				var done = this.async();
 				if (!input) {
 					done('You need to provide a module name.');
 					return;
+				} else if (typeof input !== 'string'){
+					done('Please enter a valid module name.');
+					return;
 				}
 				done(true);
-			},
-			name: 'mod'
+			}
 		},
 		{
+			message: 'Which resources would you like?',
+			name: 'choices',
+			type: 'checkbox'
 			choices: 
 				['service',
 				'directive', 
 				'routes',
 				'view',
 				'controller'],
-			message: 'Which resources would you like?',
-			name: 'choices',
-			type: 'checkbox'
 		}], function( answers ) {
 			answers.choices.push('register');
 			app(answers.mod, answers.choices);
